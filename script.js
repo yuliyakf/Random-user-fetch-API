d376479b3d68f338c88c7257cc0ea696f5eab1cf
Yuliya <yuliyakf@gmail.com>
2023-06-15 19:03:36 -0400

    final updates

---
+++ script.js
@@ -0,0 +1,56 @@
+function fetchUser(){
+    showSpinner();
+    fetch('https://randomuser.me/api/')
+    .then((res)=> res.json())  //with the first .then we get a response and we want to return res.json()
+    .then((data) =>{   //and what is returned will go in the 2nd .then() and we want data
+        hideSpinner();
+        displayUser(data.results[0]);
+    })
+}
+
+function displayUser(user){
+
+    const userDisplay = document.querySelector('#user');
+
+    console.log(user.gender)
+    if(user.gender === 'female'){
+        document.body.style.backgroundColor= "pink"
+    } else {
+        document.body.style.backgroundColor= "steelblue"
+    }
+
+    userDisplay.innerHTML = ` 
+    <div class="flex justify-between">
+          <div class="flex">
+            <img
+              class="w-48 h-48 rounded-full mr-8"
+              src="${user.picture.large}"
+            />
+            <div class="space-y-3">
+              <p class="text-xl">
+                <span class="font-bold">Name: </span>${user.name.first} ${user.name.last}
+              <p class="text-xl">
+                <span class="font-bold">Email: </span>${user.email}
+              </p>
+              <p class="text-xl">
+                <span class="font-bold">Phone: </span>${user.phone}
+              </p>
+              <p class="text-xl">
+                <span class="font-bold">Location: </span>${user.location.city} ${user.location.country}
+              </p>
+              <p class="text-xl"><span class="font-bold">Age: </span>${user.registered.age}</p>
+            </div>
+          </div>
+    `
+}
+
+function showSpinner(){
+    document.querySelector('.spinner').style.display ='block';
+}
+
+function hideSpinner(){
+    document.querySelector('.spinner').style.display ='none';
+}
+document.querySelector("#generate").addEventListener("click", fetchUser);
+
+fetchUser();
